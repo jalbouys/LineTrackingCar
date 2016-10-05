89ac00266e6016575d4f13e2d50b5d7c595db42b
Louis-Marie <louismarie.dallen@gmail.com>
2016-10-05 08:46:59 +0200

    communication working

--- Car_week_1/Communication.ino
+++ Car_week_1/Communication.ino
@@ -1,5 +1,23 @@
 void getData()//get data from Raspberry Pi
 {
-  
+
+  String readString="";
+  while (Serial.available()) {
+      delay(10);
+      if (Serial.available() > 0) {
+          char c = Serial.read(); // Gets one byte from serial buffer
+          readString += c; // Makes the string readString
+      }
+    }
+
+    if (readString.length() > 0) {
+        Serial.println(readString); // See what was receive
+        digitalWrite(LED_PIN, HIGH); // Turn the LED on (HIGH is the voltage level).
+        delay(1000);             // Wait for one second.
+        digitalWrite(LED_PIN, LOW);  // Turn the LED off by making the voltage LOW.
+        delay(1000);  
+        
+    }
+   
 }
 
